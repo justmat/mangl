5b4df016bc6df0a691acd14c0f8da30eb8b4411b
justmat <justmat@mykolab.com>
2019-12-03 22:52:34 +0700

    screen improvements

--- mangl.lua
+++ mangl.lua
@@ -395,7 +395,11 @@ function redraw()
   else
     local long_name = string.match(params:get(track .. "sample"), "[^/]*$")
     local short_name = string.match(long_name, "(.+)%..+$")
-    screen.text_right(short_name)
+    local final_name = short_name
+    if string.len(short_name) >= 15 then
+      final_name = string.sub(short_name, 1, 4) .. '...' .. string.sub(short_name, -4)
+    end
+    screen.text_right(final_name)
   end
 
   screen.move(64, 36)
